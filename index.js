fbde4a06b6543aaba2bffa67493908cb3522ba10
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-11-30 22:54:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const User = require("./database");
 
 app.set("view engine","ejs");
 
-app.use(express.urlencoded({extended:false}))
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 
 app.get("/",async(req,res)=>{
